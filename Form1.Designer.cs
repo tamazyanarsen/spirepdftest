e47f7bc19336bdf344442bf09bca2ceabefff6bf
tamazyanarsen <tamazyanarsen@mail.ru>
2019-09-08 10:32:30 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace spirepdfapi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pdfViewer1 = new Spire.PdfViewer.Forms.PdfViewer();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pdfViewer2 = new Spire.PdfViewer.Forms.PdfViewer();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(141, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // pdfViewer1
+            // 
+            this.pdfViewer1.FindTextHighLightColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(153)))), ((int)(((byte)(193)))), ((int)(((byte)(218)))));
+            this.pdfViewer1.IgnoreCase = false;
+            this.pdfViewer1.IsToolBarVisible = true;
+            this.pdfViewer1.Location = new System.Drawing.Point(25, 57);
+            this.pdfViewer1.MultiPagesThreshold = 60;
+            this.pdfViewer1.Name = "pdfViewer1";
+            this.pdfViewer1.Size = new System.Drawing.Size(0, 0);
+            this.pdfViewer1.TabIndex = 3;
+            this.pdfViewer1.Text = "pdfViewer1";
+            this.pdfViewer1.Threshold = 60;
+            this.pdfViewer1.ViewerBackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(229)))), ((int)(((byte)(229)))));
+            this.pdfViewer1.Click += new System.EventHandler(this.PdfViewer1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.OpenFileDialog1_FileOk);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(319, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 39);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Open image";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // pdfViewer2
+            // 
+            this.pdfViewer2.FindTextHighLightColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(153)))), ((int)(((byte)(193)))), ((int)(((byte)(218)))));
+            this.pdfViewer2.IgnoreCase = false;
+            this.pdfViewer2.IsToolBarVisible = true;
+            this.pdfViewer2.Location = new System.Drawing.Point(36, 82);
+            this.pdfViewer2.MultiPagesThreshold = 60;
+            this.pdfViewer2.Name = "pdfViewer2";
+            this.pdfViewer2.Size = new System.Drawing.Size(878, 609);
+            this.pdfViewer2.TabIndex = 5;
+            this.pdfViewer2.Text = "pdfViewer2";
+            this.pdfViewer2.Threshold = 60;
+            this.pdfViewer2.ViewerBackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(229)))), ((int)(((byte)(229)))));
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 821);
+            this.Controls.Add(this.pdfViewer2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pdfViewer1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private Spire.PdfViewer.Forms.PdfViewer pdfViewer1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private Spire.PdfViewer.Forms.PdfViewer pdfViewer2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using Spire.Pdf;
+using Spire.Pdf.Graphics;
+using Spire.Pdf.HtmlConverter;
+using System;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace spirepdfapi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Width = 900;
+            pdfViewer2.Width = Convert.ToInt32(this.Width * 0.9);
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        void println(object s) => Console.WriteLine(s.ToString());
+
+        async private void Button1_Click(object sender, EventArgs e)
+        {
+            string URL = textBox1.Text;
+            string pathToFile = "";
+            await Task.Run(() =>
+            {
+                pathToFile = createPdfDocFromURL(URL, "docFromUrl");
+            });
+            pdfViewer2.LoadFromFile(pathToFile);
+        }
+
+        private void PdfViewer1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        String createPdfDocFromURL(string url, string outputName)
+        {
+            //Create a pdf document.
+            PdfDocument doc = new PdfDocument();
+
+            PdfPageSettings setting = new PdfPageSettings();
+
+            setting.Size = new SizeF(1000, 1000);
+            setting.Margins = new Spire.Pdf.Graphics.PdfMargins(20);
+
+            PdfHtmlLayoutFormat htmlLayoutFormat = new PdfHtmlLayoutFormat();
+            htmlLayoutFormat.IsWaiting = true;
+
+            Thread thread = new Thread(() =>
+            { doc.LoadFromHTML(url, false, false, false, setting, htmlLayoutFormat); });
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+            thread.Join();
+            //Save pdf file.
+            doc.SaveToFile($"{outputName}.pdf");
+            doc.Close();
+            return $"{outputName}.pdf";
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        async private void OpenFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            await Task.Run(() =>
+            {
+                // Create a pdf document with a section and page added.
+                PdfDocument doc = new PdfDocument();
+                PdfSection section = doc.Sections.Add();
+                PdfPageBase page = doc.Pages.Add();
+                //Load a tiff image from system
+                PdfImage image =
+                    PdfImage.FromFile(
+                        System.Text.RegularExpressions.Regex.Escape(
+                            openFileDialog1.FileName).Replace(@"\.", "."));
+                //Set image display location and size in PDF
+                float widthFitRate = image.PhysicalDimension.Width / page.Canvas.ClientSize.Width;
+                float heightFitRate = image.PhysicalDimension.Height / page.Canvas.ClientSize.Height;
+                float fitRate = Math.Max(widthFitRate, heightFitRate);
+                float fitWidth = image.PhysicalDimension.Width / fitRate;
+                float fitHeight = image.PhysicalDimension.Height / fitRate;
+                page.Canvas.DrawImage(image, 30, 30, fitWidth, fitHeight);
+                doc.SaveToFile("imageToPdf.pdf");
+                doc.Close();
+            });
+            pdfViewer2.LoadFromFile("imageToPdf.pdf");
+        }
+    }
+}
